07cef12623c8d90ba7b569fbf9631e4720b3079d
MylesJones <myles.jones@atos.net>
2021-07-14 13:17:07 +0100

    Added a homepage and changed the url for the game in an attempt to make input easier

--- sudoku.py
+++ sudoku.py
@@ -1,18 +1,26 @@
 from flask import Flask, render_template, request
 from puzzle import *
+import random
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    if request.method == 'GET':
-        game = Puzzle()
-        return render_template("index.html", grid = game.grid)
-    if request.method == 'POST':
-        data = request.form
-        #update grid with new input
-        print(data)
-        return render_template("index.html", grid=game.grid)
+    n = random.randrange(10000)
+    return render_template("index.html", n = n)
+    
+
+
+@app.route("/game/<n>", methods=['GET', 'POST'])
+def game(n):
+    game = Puzzle(int(n))
+    data = request.form
+    #update grid with new input
+    print(data)
+    return render_template("game.html", grid=game.grid)
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
