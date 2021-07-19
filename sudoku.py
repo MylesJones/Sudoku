6d3a76dd60050682bda4c54b71f237a46cc9d582
MylesJones <myles.jones@atos.net>
2021-07-19 15:52:57 +0100

    One new comment

--- sudoku.py
+++ sudoku.py
@@ -9,7 +9,7 @@ def index():
     n = random.randrange(10000)
     return render_template("index.html", n = n)
     
-
+#test with /1643 -- 781349562   395276418   246815379   469182753   123567894   857493621   918654237   534721986   672938145
 
 @app.route("/game/<n>", methods=['GET', 'POST'])
 def game(n):
@@ -19,11 +19,15 @@ def game(n):
     #update grid with new input, check if grid is full.
     game.grid, isFull = updateGrid(game, data)
 
+
+
     if isFull:
         if game.isLegal():
             return render_template("complete.html", n=n)
     
     return render_template("game.html", grid=game.grid, templateGrid = templateGame.grid)
+
+    
 
 def updateGrid(game, data):
     isFull = True
